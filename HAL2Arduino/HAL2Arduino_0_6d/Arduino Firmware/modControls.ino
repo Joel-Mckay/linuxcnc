f2c151bec14eb556caa00d5d7a335f331f948ec7
¨Anonymous¨ <you@example.com>
2018-05-02 04:33:05 -0700

    HAL for infinity platform testing

---
+++ HAL2Arduino/HAL2Arduino_0_6d/Arduino Firmware/modControls.ino
@@ -0,0 +1,43 @@
+#define useControls true //Enable/Disable support for control panel button switches.
+// This modFile supports basic user input controls
+#if useControls
+  #define eStopSwitchPin 26
+  #define machinePowerSwitchPin 27
+  #define startSwitchPin 30
+  #define programStepSwitchPin 0
+  #define pauseSwitchPin 0
+  #define stopSwitchPin 29
+
+  #define eStopIndicatorPin 0
+  #define machineIndicatorSwitchPin 0
+  #define startIndicatorPin 0
+  #define programStepIndicatorPin 0
+  #define pauseIndicatorPin 0
+  #define stopIndicatorPin 0
+
+void contolsSetup(){
+  pinMode(eStopSwitchPin,INPUT);
+  pinMode(machinePowerSwitchPin,INPUT);
+  pinMode(startSwitchPin,INPUT);
+  
+  //~ pinMode(programStepSwitchPin,INPUT);
+  //~ pinMode(pauseSwitchPin,INPUT);
+  
+  pinMode(stopSwitchPin,INPUT);
+  pinMode(eStopIndicatorPin,OUTPUT);
+	
+  //~ pinMode(machineIndicatorSwitchPin,OUTPUT);
+  //~ pinMode(startIndicatorPin,OUTPUT);
+  //~ pinMode(programStepIndicatorPin,OUTPUT);
+  //~ pinMode(pauseIndicatorPin,OUTPUT);
+  //~ pinMode(stopIndicatorPin,OUTPUT);
+	
+  digitalWrite(eStopSwitchPin,HIGH);
+  digitalWrite(machinePowerSwitchPin,HIGH);
+  digitalWrite(startSwitchPin,HIGH);
+  //~ digitalWrite(programStepSwitchPin,HIGH);
+  //~ digitalWrite(pauseSwitchPin,HIGH);
+  digitalWrite(stopSwitchPin,HIGH);
+}
+#endif
+
